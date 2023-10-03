4015fc03c3f7bed9e1c7098a74863475d344f14f
zelstart <e.pomeroy95@gmail.com>
2023-10-02 23:13:45 -0400

    just restructured the folders

--- server.js
+++ server.js
@@ -32,8 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// routes here
-// app.use('/', htmlRoutes) 
+
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
