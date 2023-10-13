28d485689e2503bea82f2ebc669e56e44f29860e
zelstart <e.pomeroy95@gmail.com>
2023-10-13 15:51:52 -0400

    comments now display on the page.

--- controllers/recipeRoutes.js
+++ controllers/recipeRoutes.js
@@ -2,26 +2,31 @@ const express = require('express');
 const router = express.Router();
 const { Recipe, User, Comment } = require('../models');
 
-
-// get 
+// get route for recipe page
 router.get('/:id', async (req, res) => {
     try {
         const recipe = await Recipe.findByPk(req.params.id, {
-            include: [{ model: Comment, include: { model: User } }]
+            include: [{ model: Comment, include: User }]
         });
 
+        if (!recipe) {
+            return res.status(404).json({ message: 'Recipe not found' });
+        }
+
+        console.log('user id:', req.session.user_id)
+        console.log(recipe.comments)
+        const recipeData = recipe.toJSON();
+        recipeData.logged_in = req.session.logged_in;
+        recipeData.user_id = req.session.user_id;
+
         res.render('recipepage', {
-            recipeName: recipe.title,
-            ingredients: recipe.ingredients,
-            instructions: recipe.instructions,
-            image: recipe.image,
-            logged_in: req.session.logged_in, 
-            recipeComments: recipe.Comments
+            recipe: recipeData,
         });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Internal Server Error' });
     }
 });
+
 
 module.exports = router;
